cb2971a93622570df34a095cb55e58e8e73bef46
toshiyuki <toshiyuki.saito.16@gmail.com>
2021-01-13 08:22:21 +0900

    #005 [wip] implements upload_image

--- server/app.py
+++ server/app.py
@@ -1,8 +1,39 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
 
 app = Flask(__name__)
-
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024　# 1MB
 
 @app.route("/")
 def hello() -> str:
     return "Hello, World!"
+
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
+  # <input type="file" name="uploadFile"
+  if 'uploadFile' not in request.files:
+      return jsonify({'error': 'uploadFile is required.'}), 400
+  file = request.files['uploadFile']
+  file_name = file.filename
+  if '' == file_name:
+      return jsonify({'error': 'filename must not empty.'}), 400
+  pass
+
+  uuid = uuidv4()
+  save_path = "" #  TODO os.path.join(UPLOAD_DIR, save_path)
+  file.save(save_path)
+  return jsonify({
+    'result': {
+      "uuid": uuid
+    }
+  })
+"""
+def grayscale():
+  request.path # uuid/original.jpg
+
+  ## ...
+  uuid/uuid.jpg
+
+  return {
+    # uuid, uuid2
+  }
+"""
