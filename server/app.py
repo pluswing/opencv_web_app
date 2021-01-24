c43ae2d6d9bee9921cdf071d9112175818b9f931
toshiyuki <toshiyuki.saito.16@gmail.com>
2021-01-25 08:10:38 +0900

    #013 implements grayscale API

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from typing import Any
+from typing import Any, Tuple
 from uuid import uuid4
 import os
 import cv2
@@ -15,6 +15,14 @@ TASK_DIR = os.path.join(
     "static", "task")
 
 
+def image_path(task_id: str, id: str) -> str:
+    return os.path.join(TASK_DIR, task_id, f"{id}.jpg")
+
+
+def error_res(message: str) -> Tuple[Any, int]:
+    return jsonify({'error': message}), 400
+
+
 @app.route("/")
 def hello() -> str:
     return "Hello, World!"
@@ -24,15 +32,15 @@ def hello() -> str:
 def upload_image() -> Any:
     # <input type="file" name="uploadFile"
     if 'uploadFile' not in request.files:
-        return jsonify({'error': 'uploadFile is required.'}), 400
+        return error_res("required upload file")
     file = request.files['uploadFile']
     file_name = file.filename
     if '' == file_name:
-        return jsonify({'error': 'filename must not empty.'}), 400
+        return error_res('filename must not empty.')
 
     task_id = str(uuid4())
     id = str(uuid4())
-    save_path = os.path.join(TASK_DIR, task_id, f"{id}.jpg")
+    save_path = image_path(task_id, id)
     os.makedirs(os.path.dirname(save_path))
     # file.save(save_path)
     img = cv2.imdecode(np.fromstring(
@@ -50,14 +58,26 @@ def upload_image() -> Any:
     })
 
 
-"""
-def grayscale():
-  request.path # uuid/original.jpg
+@app.route('/grayscale', methods=['POST'])
+def grayscale() -> Any:
+    data = request.json
+    # {task_id: XXX, id: XXX}
+    task_id = data.get("task_id", "")
+    path = image_path(task_id, data.get("id", ""))
+    if not os.path.exists(path):
+        error_res('filename not exists')
 
-  ## ...
-  uuid/uuid.jpg
+    img = cv2.imread(path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    new_id = str(uuid4())
+    write_path = image_path(task_id, new_id)
+    cv2.imwrite(write_path, gray)
 
-  return {
-    # uuid, uuid2
-  }
-"""
+    return jsonify({
+        'result': {
+            "image": {
+                "task_id": task_id,
+                "id": new_id,
+            }
+        }
+    })
